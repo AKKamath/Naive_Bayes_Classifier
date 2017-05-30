d027bcb6f7a0f25974a6090382f0c5799162589c
AdityaK <a_kamath@hotmail.com>
2017-05-30 11:50:01 +0530

    Improve documentation

--- Hash_Tagger.cpp
+++ Hash_Tagger.cpp
@@ -19,21 +19,29 @@ bool fileExists(const char *fileName)
     return infile.good();
 }
 
+
 // Used to store tags
-class converter
+class TagStorage
 {
+	// Stores the tags
+	// string is the tagName, TAG_SIZE is the ID
 	map<string, TAG_SIZE> tag;
 	public:
+	// Used to read from file and create tags
 	void init(string file);
+	// Access tags by string name
 	TAG_SIZE operator [] (string tag_name)
 	{
 		if(tag[tag_name] == 0)
 			return -1;
 		return tag[tag_name];
 	}
+	// Output all tags
 	void print();
 };
-void converter::print()
+
+// Output all tags and details
+void TagStorage::print()
 {
 	cout<<"Name\tTag\n";
 	for(map<string, TAG_SIZE>::iterator it = tag.begin(); it != tag.end(); ++it)
@@ -41,8 +49,9 @@ void converter::print()
 		cout<<it->first<<"\t"<<it->second<<"\n";
 	}
 }
+
 // Read tags from file and assign values
-void converter::init(string file)
+void TagStorage::init(string file)
 {
 	if(!fileExists(file.c_str()))
 	{
@@ -51,25 +60,30 @@ void converter::init(string file)
 	}
 	ifstream f(file.c_str(), ios::in);
 	int i = 1;
+	// Read tag name, increment ID and store
 	string s;
 	while(getline(f, s))
 		tag[s] = i++;
 }
 
-// Read
-void assign_vals(string directory, converter tags)
+// Read training data from files
+void assignValues(string directory, TagStorage tags)
 {
+	// Start from 0.txt
 	int i = 0;
+	// We found a save file, save time and start from where we left off?
 	if(fileExists((directory + "save_state.sav").c_str()))
 	{
 		cout<<"Save found, continue from this location? (1 = Yes, 0 = No)\n";
 		int opt;
 		cin>>opt;
+		// Nah, delete all saved data and restart
 		if(opt == 0)
 		{
 			remove((directory + "save_state.sav").c_str());
 			system("rm -r Data");
 		}
+		// Resume from last saved location
 		else
 		{
 			ifstream in((directory + "save_state.sav").c_str());
@@ -80,27 +94,31 @@ void assign_vals(string directory, converter tags)
 	string word;
 	for(;; ++i)
 	{
-		
+		// No more files of required format found		
 		if(!fileExists((directory + to_string(i) + ".txt").c_str()) || !fileExists((directory + to_string(i) + ".key").c_str()))
 		{
 			cout<<"Reached "<<i<<".txt\n";
 			break;
 		}
-		ifstream f((directory + to_string(i) + ".txt").c_str(), ios::in);
-		ifstream vals((directory + to_string(i) + ".key").c_str(), ios::in);
+		// Open required files
+		ifstream main_file((directory + to_string(i) + ".txt").c_str(), ios::in);
+		ifstream tag_file((directory + to_string(i) + ".key").c_str(), ios::in);
 		vector<TAG_SIZE> v;
 		map<string, int> counter;
 		string s;
-		while(getline(vals, s))
+		// Read tags from file
+		while(getline(tag_file, s))
 		{
 			if(tags[s] != -1)
 				v.push_back(tags[s]);
 		}
 		bool skip = false;
-		while(!f.eof())
+		while(!main_file.eof())
 		{
+			// Read character by character
 			char l;
-			f.read((char *)&l, sizeof(l));
+			main_file.read((char *)&l, sizeof(l));
+			// Ignore lines within <> and punctuation
 			if(l == '>')
 				skip = false;
 			else if(l == '<')
@@ -109,6 +127,7 @@ void assign_vals(string directory, converter tags)
 				continue;
 			if(l == '\'')
 				continue;
+			// Completed a word, so we store it
 			if(!isalpha(l))
 			{
 				if(word.length() != 0)
@@ -117,24 +136,29 @@ void assign_vals(string directory, converter tags)
 				}
 				word.clear();
 			}
+			// Didn't complete a word, so concatenate last letter to current word
 			else
 			{
 				word += tolower(l);
 			}
 		}
+		// Check if Data directory exists, if not create it
 		if (!(stat("./Data", &sb) == 0 && S_ISDIR(sb.st_mode)))
 		{
 			system("mkdir Data");
 		}
+		// Store all words encountered
 		for(map<string, int>::iterator c_iter = counter.begin(); c_iter != counter.end(); ++c_iter)
 		{
 			map<TAG_SIZE, int> inp;
 			string path = ("Data/" + c_iter->first + ".word");
+			// Word has been encountered before, increment tags for that word
 			if(fileExists(path.c_str()))
 			{
 				ifstream infile(path.c_str(), ios::in | ios::binary);
 				TAG_SIZE t;
 				int val;
+				// Read values for each tag
 				while(infile.read((char *) &t, sizeof(t)))
 				{
 					infile.read((char *) &val, sizeof(val));
@@ -142,9 +166,11 @@ void assign_vals(string directory, converter tags)
 				}
 				infile.close();
 			}
+			// Increment values for each tag
 			for(vector<TAG_SIZE>::iterator it = v.begin(); it != v.end(); ++it)
 				inp[*it] += c_iter->second;
 			ofstream outfile(path.c_str(), ios::out | ios::binary);
+			// Write back new values to the file
 			for(map<TAG_SIZE, int>::iterator it = inp.begin(); it != inp.end(); ++it)
 			{
 				outfile.write((char *) &it->first, sizeof(it->first));
@@ -152,8 +178,9 @@ void assign_vals(string directory, converter tags)
 			}
 			outfile.close();
 		}
-		f.close();
-		vals.close();
+		main_file.close();
+		tag_file.close();
+		// Create a save state
 		ofstream out((directory + "save_state.sav").c_str(), ios::out);
 		out<<i;
 	}
@@ -166,10 +193,10 @@ int main(int argv, char *argc[])
 		cout<<"Argument format: Vocabulary Test_Location\n";
 		return 1;
 	}
-	converter tag;
+	TagStorage tag;
 	tag.init(argc[1]);
 	//tag.print();
-	assign_vals(argc[2], tag);
+	assignValues(argc[2], tag);
 	return 0;
 }
 
--- Hash_Tester.cpp
+++ Hash_Tester.cpp
@@ -14,23 +14,28 @@ bool fileExists(const char *fileName)
     ifstream infile(fileName);
     return infile.good();
 }
+// Used to sort tags by probability
 bool sorter(pair<TAG_SIZE, int> a, pair<TAG_SIZE, int> b)
 {
 	return a.second > b.second;
 }
+
 // Used to store tags
-class converter
+class TagStorage
 {
 	map<string, TAG_SIZE> tag;
 	map<TAG_SIZE, string> tag2;
 	public:
+	// Initialize tags from file
 	void init(string file);
+	// Retrieve tag by name
 	TAG_SIZE operator [] (string tag_name)
 	{
 		if(tag[tag_name] == 0)
 			return -1;
 		return tag[tag_name];
 	}
+	// Retrieve tag by ID
 	string operator [](unsigned short val)
 	{
 		return tag2[val];
@@ -38,8 +43,9 @@ class converter
 };
 
 // Read tags from file and assign values
-void converter::init(string file)
+void TagStorage::init(string file)
 {
+	// Tag file doesn't exist
 	if(!fileExists(file.c_str()))
 	{
 		cout<<"Error, "<<file<<" does not exist!\n";
@@ -48,6 +54,7 @@ void converter::init(string file)
 	ifstream f(file.c_str(), ios::in);
 	int i = 1;
 	string s;
+	// Increment ID and store
 	while(getline(f, s))
 	{
 		tag2[i] = s;
@@ -55,21 +62,22 @@ void converter::init(string file)
 	}
 }
 
-
-
-// Read
-void assign_vals(string directory, converter tags)
+// Read from training directory and create tag files
+void assignValues(string directory, TagStorage tags)
 {
 	int i = 0;
+	// We found a save, save time and continue?
 	if(fileExists((directory + "save_state.sav").c_str()))
 	{
 		cout<<"Save found, continue from this location? (1 = Yes, 0 = No)\n";
 		int opt;
 		cin>>opt;
+		// Nah, delete all data and restart
 		if(opt == 0)
 		{
 			remove((directory + "save_state.sav").c_str());
 		}
+		// Continue from where we left off
 		else
 		{
 			ifstream in((directory + "save_state.sav").c_str());
@@ -82,18 +90,22 @@ void assign_vals(string directory, converter tags)
 	for(;; ++i)
 	{
 		map<TAG_SIZE, int> possible_tags;
+		// File of required format doesn't exist, so we can stop
 		if(!fileExists((directory + to_string(i) + ".txt").c_str()))
 		{
 			cout<<"Reached "<<i<<".txt\n";
 			break;
 		}
-		ifstream f((directory + to_string(i) + ".txt").c_str(), ios::in);
-		ofstream vals((directory + to_string(i) + ".key").c_str(), ios::out);
+		// Open required files
+		ifstream main_file((directory + to_string(i) + ".txt").c_str(), ios::in);
+		ofstream tag_file((directory + to_string(i) + ".key").c_str(), ios::out);
 		bool skip = false;
-		while(!f.eof())
+		while(!main_file.eof())
 		{
+			// Read character from file
 			char l;
-			f.read((char *)&l, sizeof(l));
+			main_file.read((char *)&l, sizeof(l));
+			// Ignore anything in <> and punctuation
 			if(l == '>')
 				skip = false;
 			else if(l == '<')
@@ -102,13 +114,16 @@ void assign_vals(string directory, converter tags)
 				continue;
 			if(l == '\'')
 				continue;
+			// Reached end of a word
 			if(!isalpha(l))
 			{
 				if(word.length() != 0)
 				{
+					// Read details of the word from file
 					string path = ("Data/" + word + ".word");
 					if(fileExists(path.c_str()))
 					{
+						// Get all the tag details
 						map<TAG_SIZE, int> inp;
 						ifstream infile(path.c_str(), ios::in | ios::binary);
 						TAG_SIZE t;
@@ -118,6 +133,8 @@ void assign_vals(string directory, converter tags)
 							infile.read((char *) &val, sizeof(val));
 							inp[t] = val;
 						}
+						// Ignore words with more than 10 tags associated with them
+						// Used to avoid common words like 'the', 'a', 'is'
 						if(inp.size() < 10)
 							for(map<TAG_SIZE, int>::iterator it = inp.begin(); it != inp.end(); ++it)
 								possible_tags[it->first] += it->second;
@@ -126,24 +143,28 @@ void assign_vals(string directory, converter tags)
 				}
 				word.clear();
 			}
+			// Append current letter to end of current word
 			else
 			{
 				word += tolower(l);
 			}
 		}
+		// Go through all tags obtained
 		vector<pair<TAG_SIZE, int> > v;
 		copy(possible_tags.begin(), possible_tags.end(), back_inserter(v));
+		// Sort tags by probability
 		sort(v.begin(), v.end(), sorter);
+		// Output top 5 tags
 		char x = 0;
 		for(vector<pair<TAG_SIZE, int> >::iterator it = v.begin(); it != v.end(); ++it)
 		{
-			vals<<tags[it->first]<<" "<<it->second<<"\n";
+			tag_file<<tags[it->first]<<" "<<it->second<<"\n";
 			if(x > 5)
 				break;
 			++x;
 		}
-		f.close();
-		vals.close();
+		main_file.close();
+		tag_file.close();
 		ofstream out((directory + "save_state.sav").c_str(), ios::out);
 		out<<i;
 	}
@@ -156,9 +177,9 @@ int main(int argv, char *argc[])
 		cout<<"Argument format: Vocabulary Test_Location\n";
 		return 1;
 	}
-	converter tag;
+	TagStorage tag;
 	tag.init(argc[1]);
-	assign_vals(argc[2], tag);
+	assignValues(argc[2], tag);
 	return 0;
 }
 
